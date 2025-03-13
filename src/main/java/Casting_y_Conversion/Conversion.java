b8c512febcabbff1bfd5b3e0a4d2982379a8ab25
Roberto Mejía <robertomejiacollazos@gmail.com>
2025-03-13 16:03:27 -0500

    Conversion

--- src/main/java/Casting_y_Conversion/Conversion.java
+++ src/main/java/Casting_y_Conversion/Conversion.java
@@ -9,49 +9,59 @@ public class Conversion {
 
     public static void main(String[] args) {
 
-        // Ejemplo #1
         // Conversión de String a int
         String numeroComoCadena = "123";
         int numeroConvertido = Integer.parseInt(numeroComoCadena);
-        System.out.println(numeroConvertido); // Salida: 123
+        System.out.println(numeroConvertido); 
+        // Salida: 123
 
-        // Ejemplo #2
+    
         // Conversión de String a double
         String decimalComoCadena = "45.67";
         double decimalConvertido = Double.parseDouble(decimalComoCadena);
-        System.out.println(decimalConvertido); // Salida: 45.67
+        System.out.println(decimalConvertido); 
+        // Salida: 45.67
+        
 
-        // Ejemplo #3
         // Conversión de int a String
         int numero = 100;
         String numeroComoCadena1 = String.valueOf(numero);
-        System.out.println(numeroComoCadena1); // Salida: "100"
+        System.out.println(numeroComoCadena1); 
+        // Salida: "100"
 
+        
         // O usando concatenación
         String otroNumeroComoCadena = numero + "";
-        System.out.println(otroNumeroComoCadena); // Salida: "100"
+        System.out.println(otroNumeroComoCadena); 
+        // Salida: "100"
 
-        // Ejemplo #4
+        
         // Conversión de double a String
         double decimal = 9.99;
         String decimalComoCadena2 = String.valueOf(decimal);
-        System.out.println(decimalComoCadena2); // Salida: "9.99"
+        System.out.println(decimalComoCadena2); 
+        // Salida: "9.99"
+        
 
         // O usando Double.toString()
         String otroDecimalComoCadena = Double.toString(decimal);
-        System.out.println(otroDecimalComoCadena); // Salida: "9.99"
+        System.out.println(otroDecimalComoCadena); 
+        // Salida: "9.99"
+        
 
-        // Ejemplo #5
         // Conversión de tipos de datos numéricos
         float numeroFloat = 10.5f;
         int numeroEntero = (int) numeroFloat; // Casteo explícito
-        System.out.println(numeroEntero); // Salida: 10
+        System.out.println(numeroEntero); 
+        // Salida: 10
 
+        
         int otroNumeroEntero = 20;
         double numeroDouble = otroNumeroEntero; // Casteo implícito
-        System.out.println(numeroDouble); // Salida: 20.0
+        System.out.println(numeroDouble); 
+        // Salida: 20.0
 
-        // Ejemplo #6
+        
         // Conversión de objetos
         class Animal {
         }
@@ -61,7 +71,7 @@ public class Conversion {
         Animal animal = new Perro(); // Casteo implícito
         Perro perro = (Perro) animal; // Casteo explícito
 
-        // Ejemplo #7
+        
         // Conversión de ArrayList a Array
         ArrayList<String> lista = new ArrayList<>();
         lista.add("uno");
@@ -70,56 +80,87 @@ public class Conversion {
 
         String[] array = lista.toArray(new String[0]);
         for (String s : array) {
-            System.out.println(s); // Salida: uno, dos, tres
+            System.out.println(s); 
         }
-        // Ejemplo #8
+        // Salida: uno, dos, tres
+        
+        
         // Conversión de tipos de datos de fecha
         Date fecha = new Date();
         LocalDate fechaLocal = fecha.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        System.out.println(fechaLocal); // Salida: fecha actual en formato LocalDate
+        System.out.println(fechaLocal); 
+        // Salida: fecha actual en formato LocalDate
         
-        // =====================================================================
-        //  MODO 1
+ 
+        //  De String a int
         String numeroString = "300";
         int num = Integer.parseInt(numeroString);
         System.out.println(num);
-
+        // Salida: 300
+        
+        
+        // De String double
         String numeroCadena = "300.5";
         double num2 = Double.parseDouble(numeroCadena);
         System.out.println(num2);
+        // Salida: 300.5
 
+        
+        // De String a boolean
         String cadenaBolean = "TRUE";
         boolean verdadero = Boolean.parseBoolean(cadenaBolean);
         System.out.println(verdadero);
+        // Salida: TRUE
+        
 
+        // De int a String
         int numeroLetras = 100;
         String num3 = Integer.toString(numeroLetras);
         System.out.println(num3);
+        // Salida: "100"
+        
 
-        // MODO 2
+        // De short a String
         short numShort = 10;
         String cadenaShort = String.valueOf(numShort);
         System.out.println(cadenaShort);
-
+        //Salida: "10"
+        
+        
+        // De int a String
         int numInteger = 100;
         String cadenaInteger = String.valueOf(numInteger);
         System.out.println(cadenaInteger);
+        // Salida: "100"
+        
 
+        // De long a String
         long numLong = 1000;
         String cadenaLong = String.valueOf(numLong);
         System.out.println(cadenaLong);
-
+        // Salida: "1000"
+        
+        
+        // De double  a String
         double numDouble = 100.5d;
         String cadenaDouble = String.valueOf(numDouble);
         System.out.println(cadenaDouble);
+        // Salida: "100.5"
 
+        
+        // De float a String
         float numFloat = 200.5f;
         String cadenaFloat = String.valueOf(numFloat);
         System.out.println(cadenaFloat);
-
+        // Salida: "200.5"
+        
+        
+        // De boolean a String
         boolean booleanVerdadero = false;
         String cadenaVerdadero = String.valueOf(booleanVerdadero);
         System.out.println(booleanVerdadero);
+        // Salida: "false"
+        
     }
 
 }
