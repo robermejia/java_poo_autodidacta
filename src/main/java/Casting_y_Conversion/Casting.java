84b0a1188520939b9d8faffb4bbdfb2988ee23ef
Roberto Mejía <robertomejiacollazos@gmail.com>
2025-03-13 07:16:24 -0500

    Pantallazos, Casting

--- src/main/java/Casting_y_Conversion/Casting.java
+++ src/main/java/Casting_y_Conversion/Casting.java
@@ -3,34 +3,45 @@ package Casting_y_Conversion;
 public class Casting {
     public static void main(String[] args) {
         
-        /* Casteo: Es un tipo específico de conversión que se refiere a la 
+        /* 
+        Caste: Es un tipo específico de conversión que se refiere a la 
         transformación de tipos de datos dentro de la jerarquía de tipos de 
         Java, ya sea de forma implícita o explícita.
+        
         Conversión: Es un término más amplio que incluye cualquier tipo de 
         transformación de datos, incluyendo el casteo, pero también abarca otras 
         transformaciones que pueden no estar relacionadas con la jerarquía de 
         tipos.
         */
         
+        // De int a double
         int numero = 100;
         double decimal = numero; // Casteo implícito
-        int otroNumero = (int) 9.99; // Casteo explícito
+        System.out.println("Int => decimal: " + decimal);
+        
+        // De double a int
+        int otroNumero = (int) 9.99; // Casteo explícito 
+        System.out.println("Double => Int: " + otroNumero);
 
+        // De int double
         int num = 10;
         double numDouble = (double) num;
-        System.out.println("Casteo exitosa: " + numDouble);
+        System.out.println("Int => Double: " + numDouble);
 
+        // De int a short
         int numInt = 30000;
         short numShort = (short) numInt;
-        System.out.println("Casteo exitosa: " + numShort);
+        System.out.println("Int => Short: " + numShort);
 
+        // De int a log
         int num2Int = 40000;
         long numLong = (long) num2Int;
-        System.out.println("Casteo exitosa: " + numLong);
+        System.out.println("Int => Log: " + numLong);
 
+        // De int a String
         // NOTA: Esto es una conversion no un casteo
         int num3Int = 50000;
         String cadenaInteger = Integer.toString(num3Int);
-        System.out.print("La conversion fue exitosa: " + cadenaInteger);
+        System.out.print("Int => Cadena: " + cadenaInteger);
     }
 }
