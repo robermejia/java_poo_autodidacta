c36702d54f62d4e236d5453d02b039e60e2810a7
Roberto Mejía <59416763+robermejia@users.noreply.github.com>
2025-03-14 18:40:53 -0500

    Exceptions, recursión,

--- src/main/java/Exceptions/Algunas_Excepciones.java
+++ src/main/java/Exceptions/Algunas_Excepciones.java
@@ -1,8 +1,8 @@
-package Exeptions;
+package Exceptions;
 
 import javax.swing.JOptionPane;
 
-public class ecepciones {
+public class Algunas_Excepciones {
     public static void main(String[] args) {
         int x, y;
         
@@ -14,8 +14,11 @@ public class ecepciones {
             y = Integer.parseInt(JOptionPane.showInputDialog(null, 
                     "Ingrese otro número"));
             JOptionPane.showConfirmDialog(null,"la division de " + x + " + " + y + " es: " + (x/y));
+            
+            // NumberFormatException e (Error al poner un caracter en lugar de un número.)
             } catch (NumberFormatException e) {
                 JOptionPane.showMessageDialog(null, "Este caracter no es admite letras.");
+            // AritmeticException e (Error al poner un cero donde no lo permite.)
             } catch (ArithmeticException c){
                 JOptionPane.showMessageDialog(null, "Este caracter no es admite cero.");
             }     
--- src/main/java/Exceptions/Throw_Throws.java
+++ src/main/java/Exceptions/Throw_Throws.java
@@ -1,14 +1,22 @@
-package Exeptions;
+package Exceptions;
 
 import javax.swing.JOptionPane;
 
-public class Throw {
+public class Throw_Throws {
 
     public static void main(String[] args) {
+        
+    // try: Bloque donde puede ocurrir la excepción.
+    // catch: Es como el "else" del try (se ejecuta al dispararse una excepción en el bloque try).
+    // finally: Bloque de código que se ejecuta siempre (sin importar si hubieron errores o no).
+    // throw: Lanzar una excepción cualquiera.
+    // throws: Determinar que excepciones puede lanzar un método.
       
     int x,y,n,m;
-        
+    
+    // Bucle de suma y división repetido 2 veces
     for (int i = 0; i < 2; i++) {
+    // try
             try {
             x = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese un número para sumarlo."));
             y = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese otro número para sumarlo."));
@@ -16,7 +24,7 @@ public class Throw {
             n = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese un número para dividirlo."));
             m = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese otro número para dividirlo."));
                 dividir(n, m);
-                
+    // catch      
             } catch (NumberFormatException e) {
                 JOptionPane.showMessageDialog(null, "Este valor es una letra y no un número.");
             } catch (ArithmeticException a) {
@@ -24,6 +32,8 @@ public class Throw {
             }
         }
     }
+    
+    // Método de suma
     public static int sumar(int num1, int num2) {
         int num3 = 0;
         num3 = num1 + num2;
@@ -31,9 +41,12 @@ public class Throw {
         return num3;
     }
     
+    // Método de división
+    // throws (Indica que tipo de excepción va tener el método "dividir")
     public static int dividir(int num1, int num2) throws ArithmeticException {
         int num3 = 0;
         if (num2 == 0) {
+        // throw (Muestra el cualquier tipo de excepción)
             throw new ArithmeticException("No se puede dividir entre cero.");
         }
         num3 = num1 / num2;
