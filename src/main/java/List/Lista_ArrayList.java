22bbe211d94cce85d5996fd20abcc93ce87b0028
Roberto Mejía <59416763+robermejia@users.noreply.github.com>
2025-03-14 21:34:12 -0500

    ArrayList, LinkedList

--- src/main/java/List/Lista_ArrayList.java
+++ src/main/java/List/Lista_ArrayList.java
@@ -17,7 +17,8 @@ public class Lista_ArrayList {
         // Sintaxis Forma 2: Cuando se hace referencia a una clase (FamiliaNuclear)
         ArrayList<FamiliaNuclear> personas2 = new ArrayList<>();
 
-        // Método "add" para añadir elemento a la lista
+        
+        // add(String/Obj) => Método para añadir elemento a la lista
         personas.add(person1);
         personas.add(person2);
         personas.add(person3);
@@ -30,6 +31,20 @@ public class Lista_ArrayList {
                     + "Ciudad: " + per.getCiudad() + "\n");
         }
         System.out.println("==================================================");
+        
+        // Salida: Nombre: Rober
+        // Salida: Apellido: Mejia
+        // Salida: Edad: 25
+        // Salida: Ciudad: Lima
+        // Salida: Nombre: Agust�n
+        // Salida: Apellido: Mejia
+        // Salida: Edad: 65
+        // Salida: Ciudad: Lima
+        // Salida: Nombre: Narda
+        // Salida: Apellido: Collazos
+        // Salida: Edad: 60
+        // Salida: Ciudad: Lima
+        // =================================================
 
         // Sintaxis Forma 3: Cuando se hace referencia a un tipo de dato, ejemplo: String
         ArrayList<String> familia = new ArrayList<>();
@@ -38,60 +53,86 @@ public class Lista_ArrayList {
         familia.add("Luisa");
         familia.add("Pedro");
         familia.add("Marcos");
-
+        
         // Imprimiendo la lista
         for (String fam : familia) {
             System.out.print("Familia: " + fam + "\n");
         }
-
+        
+        // Salida: Familia: Luisa
+        // Salida: Familia: Pedro
+        // Salida: Familia: Marcos
+        // =================================================
+        System.out.println("=================================================");
         
         // Ejemplos de métodos
-        // Añadir un elemento a la lista
+        // add(String/obj) => Añadir un elemento a la lista
         familia.add("Luis");
         System.out.println(familia);
+        // Salida: [Luisa, Pedro, Marcos, Luis]
+        // =================================================
         System.out.println("=================================================");
 
-        // Añadir un elemento a la lista en un posición específica "3"
+        // add(index, obj) => Añadir un elemento a la lista en un posición específica "3"
         familia.add(3, "Pedro");
         System.out.println(familia);
+        // Salida: [Luisa, Pedro, Marcos, Pedro, Luis]
+        // =================================================
         System.out.println("=================================================");
 
-        // Tamaño de la lista
+        // .size() => Tamaño de la lista
         System.out.println("Tamaño de la lista: " + familia.size());
+        // Salida: Tama�o de la lista: 5
+        //=================================================
+
         System.out.println("=================================================");
         
-        // Obtener elemento de la lista
+        // .get(index) => Obtener elemento de la lista
         System.out.println("Elemento en posición 2: " + familia.get(2));
+        // Salida: Elemento en posici�n 2: Marcos
+        //=================================================
         System.out.println("=================================================");
 
-        // Comprobando si contiene un elemento
+      
+        // .constain(obj) => Comprobando si contiene un elemento
         System.out.println("¿Contiene 'Pedro'? " + familia.contains("Pedro"));
+        // Salida: ¿Contiene 'Pedro'? true
+        //=================================================
         System.out.println("=================================================");
 
-        // Removiendo un elemento
+        // .remove(index) => Removiendo un elemento
         familia.remove(4);
         System.out.println(familia);
+        // Salida: [Luisa, Pedro, Marcos, Pedro]
+        //=================================================
         System.out.println("=================================================");
 
-        // Limpiando la lista
+        // .clear() => Limpiando la lista
         familia.clear();
         System.out.println("Lista después de limpiar: " + familia);
+        // Salida: Lista despu�s de limpiar: []
+        //=================================================
         System.out.println("=================================================");
 
-        // Comprobando si está vacía
+        // .isEmpty() => Comprobando si está vacía
         System.out.println("¿Está vacía? " + familia.isEmpty());
+        //Salida: ¿Est� vacía? true
+        //=================================================
         System.out.println("=================================================");
 
-        // Copiando un ArrayList
+        // .clone() => Copiando un ArrayList
         ArrayList<String> arrayListCopia = (ArrayList<String>) familia.clone();
         System.out.println("Copia de la lista: " + arrayListCopia);
+        // Salida: Copia de la lista: []
+        //=================================================
         System.out.println("=================================================");
 
-        // Pasando el ArrayList a un Array
+        // .toArray() => Pasando el ArrayList a un Array
         FamiliaNuclear[] famArray = personas.toArray(new FamiliaNuclear[0]);
-
         // Convertir el array a una cadena (usando Arrays.toString)
         System.out.println("Array: " + Arrays.toString(famArray));
+        // Salida: Array: [Rober Mejia (25, Lima), Agust�n Mejia (65, Lima), Narda Collazos (60, Lima)]
+        //=================================================
         System.out.println("=================================================");
     }
 }
--- src/main/java/List/Lista_LinkedList.java
+++ src/main/java/List/Lista_LinkedList.java
@@ -7,17 +7,17 @@ public class Lista_LinkedList {
     
         public static void main(String[] args) {
         
-        // Modo 1
+        // Instancia (Modo 1)
         LinkedList<String> LibrosLinkedList1 = new LinkedList<String>();
         LinkedList<String> LibrosLinkedList2 = new LinkedList<String>();
         
-        // Modo 2
+        // Instancia (Modo 2)
         List<String> MusicaLinkedList1 = new LinkedList<String>();
         List<String> MusicaLinkedList2 = new LinkedList<String>();
         
         // Usando Métodos más conocidos
-        // .add()
         System.out.println("==================== MÉTODOS ====================");
+        // .add() => Añade un nuevo elementoa a la lista
         System.out.println("add(Novelas)=====================================");
         LibrosLinkedList1.add("Novelas");
         System.out.println("LibrosLinkedList1 = " + LibrosLinkedList1);
@@ -30,63 +30,63 @@ public class Lista_LinkedList {
         System.out.println("add(1,Cómic)=====================================");
         LibrosLinkedList1.add(1,"Cómics");
         System.out.println("LibrosLinkedList1 = " + LibrosLinkedList1);
-        // .add(index, obj)
+        // .add(index, obj) => Añade nuevo elemento a la lista dándole un indice 
         System.out.println("add(0,Novelas)===================================");
         LibrosLinkedList1.add(0,"Novelas");
         System.out.println("LibrosLinkedList1 = " + LibrosLinkedList1);
-        // .add(index, obj)
+        // .add(index, obj) => Añade nuevo elemento a la lista dándole un indice 
         System.out.println("add(1,Arte)======================================");
         LibrosLinkedList1.add(1,"Arte");
         System.out.println("LibrosLinkedList1 = " + LibrosLinkedList1);
-        // .addFirst(obj)
+        // .addFirst(obj) => Añade un elemento al inicio de la lista
         System.out.println("firstIndexOf('No ficción')========================");
         LibrosLinkedList1.addFirst("No ficción");
         System.out.println("LibrosLinkedList1.addFirst('No ficción') = " + LibrosLinkedList1);
-        // .addAll()
-        LibrosLinkedList2.addAll(LibrosLinkedList1);
+        // .addAll() => Añade toda la lista
         System.out.println("addAll()=========================================");
+        LibrosLinkedList2.addAll(LibrosLinkedList1);
         System.out.println("LibrosLinkedList2" + LibrosLinkedList2);
-        // .get(index)
+        // .get(index) => Muestra el elemento en el indice especificado
         System.out.println("get(0)===========================================");
         String libro = LibrosLinkedList1.get(0);
         System.out.println("LibrosLinkedList1.get(0) = " + libro);
-        // .set(index, obj)
+        // .set(index, obj) => inserta un elemento en el indice especifivcado
         System.out.println("set(0, 'No ficción')=============================");
         LibrosLinkedList1.set(0, "No ficción");
         System.out.println("LibrosLinkedList1.set(0,'No ficción') = " + LibrosLinkedList1);
-        // .remove(index)
+        // .remove(index) => Elimina un elemento en el indice especificado
         System.out.println("remove(0)========================================");
         LibrosLinkedList1.remove(0);
         System.out.println("LibrosLinkedList1.remove('No ficción') = " + LibrosLinkedList1);
-        // .remove(obj)
+        // .remove(obj) => Elimina el elemento dado
         System.out.println("remove('No ficción')=============================");
         LibrosLinkedList1.remove("No ficción");
         System.out.println("LibrosLinkedList1.remove('No ficción') = " + LibrosLinkedList1);
-        // .size()
+        // .size() => Muestra el tamaño de la lista
         System.out.println("size()===========================================");
         Integer libro1 = LibrosLinkedList1.size();
         System.out.println("LibrosLinkedList1.size() = " + libro1);
-        // .contains()
+        // .contains() => Muestra si un elemento esta en la lista con un tru o false
         System.out.println("contains('No ficción')===========================");
         Boolean libro2 = LibrosLinkedList1.contains("No ficción");
         System.out.println("LibrosLinkedList1.contains('No ficción') = " + libro2);
-        // .indexOf()
+        // .indexOf() => Muestra el indice donde se encuentra el elemento y si no se encuentra muestra -1
         System.out.println("indexOf('No ficción')============================");
         Integer libro3 = LibrosLinkedList1.indexOf("No ficción");
         System.out.println("LibrosLinkedList1.indexOf('No ficción') = " + libro3);
-        // .lastIndexOf()
+        // .lastIndexOf() => Muestra el ultimo indice de la lista
         System.out.println("lastIndexOf('No ficción')========================");
         Integer libro4 = LibrosLinkedList1.lastIndexOf("No ficción");
         System.out.println("LibrosLinkedList1.lastIndexOf('No ficción') = " + libro4);
-        // .toArray()
+        // .toArray() => Convierte a un array la lista
         System.out.println("toArray()========================================");
         Object[] libro6 = LibrosLinkedList1.toArray();
         System.out.println("LibrosLinkedList1.toArray() = " + libro6);
-        // .clear()
+        // .clear() => limpia o elimina toda la lista
         System.out.println("clear()==========================================");
         LibrosLinkedList1.clear();
         System.out.println("LibrosLinkedList1.clear() = " + LibrosLinkedList1);
-        // .isEmpty()
+        // .isEmpty() => Muestra true o false si la lista esta vacia
         System.out.println("isEmpty()========================================");
         Boolean libro7 = LibrosLinkedList1.isEmpty();
         System.out.println("LibrosLinkedList1.isEmpty() = " + libro7);
